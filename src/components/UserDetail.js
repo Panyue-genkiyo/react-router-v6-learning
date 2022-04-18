1dc83432948fb2b587b5a4921d39ce05a516b679
panyue <90938238g@gmail.com>
2022-04-18 23:16:26 +0800

    useParams hook获取params参数(比如/user/:id)中的id

--- src/components/UserDetail.js
+++ src/components/UserDetail.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useParams } from "react-router-dom";
 
 const UserDetail = () => {
+    const { id } = useParams();
     return (
         <div>
-            Detail about user
+            Detail about user {id}
         </div>
     );
 };
