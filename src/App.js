bc1b0c27158d86601b09ee729bf87e16049a29cc
panyue <90938238g@gmail.com>
2022-04-18 22:25:24 +0800

    增加配置组件OrderSummary的路由

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import About from './components/About';
 import NavBar from "./components/NavBar";
+import OrderSummary from "./components/OrderSummary";
 
 //app组件主要配置路由
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
             <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='about' element={<About/>}/>
+                <Route path='order-summary' element={<OrderSummary/>}/>
             </Routes>
         </>
     );
