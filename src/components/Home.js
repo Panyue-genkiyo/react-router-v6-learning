55c34cc1976601871ee1135238346837f6855c5f
panyue <90938238g@gmail.com>
2022-04-18 22:33:12 +0800

    home组件

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,17 @@ const Home = () => {
         <>
             <h1>home page</h1>
             {/*第二个配置参数{replace: true}直接覆盖掉上一次的记录*/}
-            <button onClick={() => navigate('order-summary', { replace: true })}>place order</button>
+            <button
+                onClick={
+                () => {
+                    navigate('order-summary',{
+                        // replace: true
+                    })
+                }
+              }
+            >
+                place order
+            </button>
         </>
     );
 };
