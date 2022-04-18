b15ff4ae69492e7a00fead6572c398e106daab59
panyue <90938238g@gmail.com>
2022-04-18 11:19:32 +0800

    NavLink的使用(会给被触发的link添加上class，active，并将传递到style回调函数里面的isActive props置为true,利用这个特性我们可以给active link加上不一样的样式)

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 //导航
+//navLink在链接被触发时添加class:active
 const NavBar = () => {
+
+    //props给导航加上active时的样式
+    //当某个link被点击时，isActive会被置为true
+    const navLinkStyles = ({ isActive }) => {
+        return {
+            fontWeight : isActive ? 'bold' : 'normal',
+            textDecoration: isActive ? 'none' : 'underline'
+        }
+    }
+
     return (
        <nav>
-           <Link to='/'>Home</Link>
-           <Link to='/about'>About</Link>
+           {/*<Link to='/'>Home</Link>*/}
+           {/*<Link to='/about'>About</Link>*/}
+           <NavLink style={navLinkStyles} to='/'>Home</NavLink>
+           <NavLink style={navLinkStyles} to='/about'>About</NavLink>
        </nav>
     );
 };
