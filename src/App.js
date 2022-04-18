fe9d5b6880f1cdca16d4aecc62b6ce2075e6ee6b
panyue <90938238g@gmail.com>
2022-04-18 22:33:59 +0800

    配置page not found路由(path='*')

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import About from './components/About';
 import NavBar from "./components/NavBar";
 import OrderSummary from "./components/OrderSummary";
+import NoMatch from "./components/NoMatch";
 
 //app组件主要配置路由
 const App = () => {
@@ -14,6 +15,8 @@ const App = () => {
                 <Route path='/' element={<Home/>}/>
                 <Route path='about' element={<About/>}/>
                 <Route path='order-summary' element={<OrderSummary/>}/>
+                {/*当前面所有路由都没有匹配时会匹配path为*的路由*/}
+                <Route path='*' element={<NoMatch/>} />
             </Routes>
         </>
     );
