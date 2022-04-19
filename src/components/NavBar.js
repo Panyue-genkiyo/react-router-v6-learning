1b12f52e20a87304aa57824c31727214391eddf9
panyue <90938238g@gmail.com>
2022-04-19 11:19:15 +0800

    navbar组件

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
+import {useAuth} from "../store";
 
 //导航
 //navLink在链接被触发时添加class:active
 const NavBar = () => {
-
+     const { user } = useAuth();
     //props给导航加上active时的样式
     //当某个link被点击时，isActive会被置为true
     const navLinkStyles = ({ isActive }) => {
@@ -21,6 +22,10 @@ const NavBar = () => {
            <NavLink style={navLinkStyles} to='/'>Home</NavLink>
            <NavLink style={navLinkStyles} to='/about'>About</NavLink>
            <NavLink style={navLinkStyles} to='/products'>Products</NavLink>
+           <NavLink style={navLinkStyles} to='/profile'>Profile</NavLink>
+           {
+               !user && <NavLink style={navLinkStyles} to='/login'>Login</NavLink>
+           }
        </nav>
     );
 };
